e335bee188b167a94445008919e5d53237098d1a
brunodprocha <brunodprocha@gmail.com>
2023-09-01 10:02:50 -0300

    Exercicio da AC4

---
+++ ACs/Ac4/Ac4.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+var contadorMovimientos int
+
+func Hanoi(n int, origem string, destino string, trabalho string) {
+	if n > 0 {
+		Hanoi(n-1, origem, trabalho, destino)
+		contadorMovimientos++
+		fmt.Printf("Movimiento %d: %s --> %s\n", contadorMovimientos, origem, destino)
+		Hanoi(n-1, trabalho, destino, origem)
+	}
+}
+
+func main() {
+	var n int
+	var A, B, C string
+	A = "A"
+	B = "B"
+	C = "C"
+	contadorMovimientos = 0
+
+	fmt.Println("Informe a quantidade de discos:")
+	fmt.Scanln(&n)
+	Hanoi(n, A, B, C)
+	fmt.Printf("Quantidade de movimentos total de %d.\n", contadorMovimientos)
+}
